f7d085229f03c4aa16df2590e134095db3ec9f06
space <space@debian.space>
2019-05-04 14:42:07 +0530

    intermediate

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -15,7 +15,41 @@ class WebhookController extends Controller
 
 		//$updates = Telegram::getWebhookUpdates();
 		$updates = Telegram::commandsHandler(true);
-		Storage::append('file.txt',json_encode($updates));
+
+		$inMessage = $updates['message'];
+		$telegram_id = $inMessage['from']['id'];
+
+
+
+		$user = \App\User::where('telegram_id',$telegram_id)->first();
+
+		if(!$user){
+			// first time message
+			$user = new \App\User;
+			$user->first_name = $inMessage['from']['first_name'];
+			$user->last_name = $inMessage['from']['last_name'];
+			$user->telegram_id = $inMessage['from']['id'];
+			$user->save();
+		}
+
+		//store the message
+
+		if(array_key_exists('document',$inMessage)){
+			$message = new \App\DocMesage;
+			$message->file_name = $inMessage['document']['file_name'];
+			$message->file_type = $inMessage['document']['mime_type'];
+			$message->file_id= $inMessage['document']['file_id'];
+			$message->file_size = $inMessage['document']['file_size'];
+
+		}else if(array_key_exists('text',$inMessage)){
+			$message = new \App\TextMessage;
+			$message->text = $inMessage['text'];
+		}
+
+		$message->tel_msg_id = $inMessage['message_id'];
+		$message->date = $message['date'];
+		$message->save();
+		$message->from()->save($user);
 
 
 		return json_encode($request);
--- app/Http/Telegram/Commands/SubmitReportCommand.php
+++ app/Http/Telegram/Commands/SubmitReportCommand.php
@@ -10,12 +10,12 @@ class StartCommand extends Command
     /**
      * @var string Command Name
      */
-    protected $name = "start";
+    protected $name = "submitReport";
 
     /**
      * @var string Command Description
      */
-    protected $description = "Start Command to get you started";
+    protected $description = "submit your daily working report";
 
     /**
      * @inheritdoc
@@ -26,6 +26,8 @@ class StartCommand extends Command
         // the user/chat id who triggered this command.
         // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
         // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
+
+        
         $this->replyWithMessage(['text' => 'Hello! Welcome to our bot, Here are our available commands:']);
 
         // This will update the chat status to typing...
