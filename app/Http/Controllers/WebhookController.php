8cd040590c2dd86137939b2a85ded03435a002f8
space <space@debian.space>
2019-05-04 18:48:46 +0530

    test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -24,43 +24,43 @@ class WebhookController extends Controller
 
 			Storage::put('file.txt',$inMessage['from']['first_name']);
 
-		$user = \App\User::where('telegram_id',$telegram_id)->first();
-
-		if(!$user){
-			// first time message
-			$user = new \App\User;
-			$user->first_name = $inMessage['from']['first_name'];
-			$user->last_name = $inMessage['from']['last_name'];
-			$user->telegram_id = $inMessage['from']['id'];
-			$user->save();
-		}
-
-		//store the message
-
-		if(array_key_exists('document',$inMessage)){
-			$message = new \App\DocMesage;
-			$message->file_name = $inMessage['document']['file_name'];
-			$message->file_type = $inMessage['document']['mime_type'];
-			$message->file_id= $inMessage['document']['file_id'];
-			$message->file_size = $inMessage['document']['file_size'];
-
-
-		}else if(array_key_exists('text',$inMessage)){
-			$message = new \App\TextMessage;
-			$message->text = $inMessage['text'];
-
-		}
-
-		$message->save();
-
-		$wrapperMessage = new \App\Message;
-		$wrapperMessage->tel_msg_id = $inMessage['message_id'];
-		$wrapperMessage->sent_on = $message['date'];
-		$wrapperMessage->save();
-
-		$message->message()->save($wrapperMessage);
-		$message->from()->save($user);
-
-		return json_encode($update);
-	}
+	// 	$user = \App\User::where('telegram_id',$telegram_id)->first();
+	//
+	// 	if(!$user){
+	// 		// first time message
+	// 		$user = new \App\User;
+	// 		$user->first_name = $inMessage['from']['first_name'];
+	// 		$user->last_name = $inMessage['from']['last_name'];
+	// 		$user->telegram_id = $inMessage['from']['id'];
+	// 		$user->save();
+	// 	}
+	//
+	// 	//store the message
+	//
+	// 	if(array_key_exists('document',$inMessage)){
+	// 		$message = new \App\DocMesage;
+	// 		$message->file_name = $inMessage['document']['file_name'];
+	// 		$message->file_type = $inMessage['document']['mime_type'];
+	// 		$message->file_id= $inMessage['document']['file_id'];
+	// 		$message->file_size = $inMessage['document']['file_size'];
+	//
+	//
+	// 	}else if(array_key_exists('text',$inMessage)){
+	// 		$message = new \App\TextMessage;
+	// 		$message->text = $inMessage['text'];
+	//
+	// 	}
+	//
+	// 	$message->save();
+	//
+	// 	$wrapperMessage = new \App\Message;
+	// 	$wrapperMessage->tel_msg_id = $inMessage['message_id'];
+	// 	$wrapperMessage->sent_on = $message['date'];
+	// 	$wrapperMessage->save();
+	//
+	// 	$message->message()->save($wrapperMessage);
+	// 	$message->from()->save($user);
+	//
+	// 	return json_encode($update);
+	 }
 }
